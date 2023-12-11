85ebaaddc47b2ab0403fa4e9a56ffd25a45ec729
fgladu <fgladu@gmail.com>
2023-12-11 15:34:46 -0500

    First commit

---
+++ sync_balcklist_to_iptables.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Check if script is run as root
+if [[ $EUID -ne 0 ]]; then
+		echo "Veuillez exécuter ce script en tant que superutilisateur (root)."
+		exit 1
+fi
+
+BANLIST_URL="https://raw.githubusercontent.com/fgladu/SRC_IP_Blacklist/main/blacklist.txt"
+CHAIN_NAME="BANLIST"
+
+# Create the chain if it doesn't exist
+iptables -N $CHAIN_NAME 2>/dev/null
+
+# Flush existing rules in the chain
+iptables -F $CHAIN_NAME
+
+# Download the banlist from the URL and read each IP to add to the BANLIST chain
+while read -r IP; do
+		if [[ -n "$IP" ]]; then
+				iptables -A $CHAIN_NAME -s $IP -j DROP
+				echo "Added $IP to $CHAIN_NAME chain."
+		fi
+done < <(curl -s "$BANLIST_URL")
+
+# Redirect traffic to the BANLIST chain
+iptables -I INPUT -j $CHAIN_NAME
+iptables -I OUTPUT -j $CHAIN_NAME
+
+# Save the iptables rules
+service iptables save
+
+echo "Adresses IP en liste noire depuis $BANLIST_URL ajoutées à iptables et enregistrées."
